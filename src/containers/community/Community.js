6cc257a4289b311d93577c5362f21adce654f8ef
yusufabdullahi2910@gmail.com <yusufabdullahi2910@gmail.com>
2023-05-15 23:53:40 +0100

    file updated

--- src/containers/community/Community.js
+++ src/containers/community/Community.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import CommunityForm from './CommunityForm'
 
 const Community = () => {
   return (
     <div>
-      <h1>Welcome to Community</h1>
+      <h1>Welcome to Community </h1>
+      <CommunityForm   />
     </div>
   )
 }
