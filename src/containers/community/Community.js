c084e0fde769255d0a1e199cfcf4fbe0c631c28c
yusufabdullahi2910@gmail.com <yusufabdullahi2910@gmail.com>
2023-05-20 19:03:33 +0100

    file uodated

--- src/containers/community/Community.js
+++ src/containers/community/Community.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import CommunityForm from './CommunityForm'
+import Join from './Join';
 
 const Community = () => {
   return (
     <div>
       <h1>Welcome to Community </h1>
+      <Join />
       <CommunityForm   />
     </div>
   )
---
+++ src/containers/community/Join.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Join from '../../assets/images/community.png';
+
+const Join = () => {
+  return (
+    <div className='Join'>
+    <img src={Join} alt="Join" />
+    <h1>Join Our Student Community</h1>
+    <p>Learn, grow and become a Digital Marketing Genius with other great minds. Be the first to get updates on our learning resources, events and opportunities.</p>
+      
+    </div>
+  )
+}
+
+export default Join;
