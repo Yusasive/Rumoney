dcac874470d220eac5c4bda7d8f32cb7fa28fbee
yusufabdullahi2910@gmail.com <yusufabdullahi2910@gmail.com>
2023-05-21 07:06:29 +0100

    file updated

--- src/containers/community/Join.js
+++ src/containers/community/Join.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import Join from '../../assets/images/community.png';
+import JoinImage from '../../assets/images/community.png';
 
 const Join = () => {
   return (
     <div className='Join'>
-    <img src={Join} alt="Join" />
+    <img src={JoinImage} alt="Join" />
     <h1>Join Our Student Community</h1>
     <p>Learn, grow and become a Digital Marketing Genius with other great minds. Be the first to get updates on our learning resources, events and opportunities.</p>
       
