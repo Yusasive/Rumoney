93bc20dab65c6823cd103f33286ebf1f53c8b753
yusufabdullahi2910@gmail.com <yusufabdullahi2910@gmail.com>
2023-07-15 22:40:49 +0100

    testing

--- src/containers/community/Join.js
+++ src/containers/community/Join.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import Col from 'react-bootstrap/Col';
 import JoinImage from '../../assets/images/community.png';
-
+import './Community.css';
 const Join = () => {
   return (
     <div className='Join'>
 
     <h2>Join Our Student Community</h2>
     <h6>Learn, grow and become a Digital Marketing Genius with other great minds. Be the first to get updates on our learning resources, events and opportunities.</h6>
-    <Col xs={6} md={4}>    <img src={JoinImage} alt="Join" />   </Col>
+   <img className='joinimage' src={JoinImage} alt="Join" />   
 
     </div>
   )
